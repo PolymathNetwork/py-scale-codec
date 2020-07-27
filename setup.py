f486aae50d5c1f6fff1ba73e0b72620c44293d02
Victor <victor.g.vicente@gmail.com>
2020-07-27 16:43:25 -0300

    Replace hyphen with underscore in package name

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='polymath-scalecodec',  # Required
+    name='polymath_scalecodec',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
